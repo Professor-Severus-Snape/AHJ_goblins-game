afcd53a13580575c416d43463f7d2bda7f3814bf
Julia Fokanova <oncologmma@mail.ru>
2024-07-08 15:56:25 +0300

    improve rights

--- src/js/App.js
+++ src/js/App.js
@@ -4,11 +4,10 @@ import Copyrights from './Copyrights';
 export default class App {
   static init() {
     const copyrights = new Copyrights();
-    if (copyrights.copyrights === '© Professor-Severus-Snape, 2024') {
-      this.game = new Game();
-      return;
+    if (copyrights.copyrights !== '© Professor-Severus-Snape, 2024') {
+      Copyrights.stoleRights();
     }
 
-    Copyrights.stoleRights();
+    this.game = new Game();
   }
 }
